9c2b0ee61c1d953d458ef26904f707cdd9f3f372
jhdo95 <jhdo95@gmail.com>
2023-09-12 12:27:15 -0700

    Part 2 Lab completion

--- main_app/views.py
+++ main_app/views.py
@@ -1,9 +1,6 @@
 from django.shortcuts import render
+from .models import Pokemon
 
-pokemons = [
-    {'name': 'pikachu', 'type': 'electric', 'description': 'yellow electric mouse' },
-    {'name': 'bulbasaur', 'type': 'grass', 'description': 'frog with flower back' },
-]
 
 # Create your views here.
 def home(request):
@@ -11,6 +8,12 @@ def home(request):
 def about(request):
     return render(request, 'about.html')
 def pokemons_index(request):
+    pokemons = Pokemon.objects.all()
     return render(request, 'pokemons/index.html', {
         'pokemons': pokemons
+    })
+def pokemons_detail(request, pokemon_id):
+    pokemon = Pokemon.objects.get(id=pokemon_id)
+    return render(request, 'pokemons/detail.html', {
+        'pokemon': pokemon
     })
